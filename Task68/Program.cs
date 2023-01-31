02d1ca0932bc59f076cf230a0b11caeb3d260859
Alavutdin <alavutdin_atlan@mail.ru>
2023-01-31 22:01:49 +0300

    Added TaskHome68

---
+++ Task68/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 68: Напишите программу реализующую метод вычисления 
+// функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+using static System.Console;
+Clear();
+
+WriteLine(GetNumbers(2,3));
+WriteLine(GetNumbers(3,2));
+
+
+int GetNumbers(int numA, int numB)
+{
+    if (numA == 0) return numB + 1;
+    else if (numB == 0) return GetNumbers(numA - 1, 1);
+    else return GetNumbers(numA - 1, GetNumbers(numA, numB - 1));
+
+}
