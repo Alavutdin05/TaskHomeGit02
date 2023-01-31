44a8a7c6a8a095039320e6062c4b110acd43e2da
Alavutdin <alavutdin_atlan@mail.ru>
2023-01-31 21:55:55 +0300

    Added TaskHome64

---
+++ Task64/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 64: Напишите программу, которая реализует рекурсивный метод вывода 
+// всех натуральных числел в промежутке от N до 1.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+using static System.Console;
+Clear();
+
+string result1 = GetSum1(5);
+WriteLine(result1);
+
+string result2 = GetSum2(8);
+WriteLine(result2);
+
+PrintNumbers(8);
+
+
+string GetSum1 (int num)
+{
+    if (num==1) return num.ToString();
+    else return $"{num},{GetSum1(num-1)}";
+}
+
+string GetSum2 (int num)                
+{
+    return (num==1)?num.ToString():$"{num},{GetSum2(num-1)}";
+}
+
+void PrintNumbers(int num)
+{
+    if(num==1) Write(num);
+    else
+    {
+        
+        Write($"{num},");
+        PrintNumbers(num-1);
+    }
+}
